90ee2469b9fb0d1413c736999c31802d57cdda58
abdulhay103 <mdabdulhay95@gmail.comm>
2023-07-03 13:26:18 +0600

    12

--- src/js/main.js
+++ src/js/main.js
@@ -11,58 +11,43 @@ function ready() {
 let products = [
     {
       id : "111",
-        details:{
-            name : "Samsung",
-            img : "./src/images/img1.avif",
-
-        },
-        unitPrice : "300",
-        qty : "2",
-        subTotal : "600",
+      name : "Samsung",
+      img : "./src/images/img1.avif",
+      unitPrice : "300",
+      qty : "2",
+      subTotal : "600",
     },
     {
       id : "112",
-        details:{
-            name : "Samsung1",
-            img : "./src/images/img2.avif",
-
-        },
-        unitPrice : "1000",
-        qty : "2",
-        subTotal : "2000",
+      name : "Samsung1",
+      img : "./src/images/img2.avif",
+      unitPrice : "1000",
+      qty : "2",
+      subTotal : "2000",
     },
     {
       id : "113",
-        details:{
-            name : "Samsung3",
-            img : "./src/images/img3.avif",
-
-        },
-        unitPrice : "1000",
-        qty : "2",
-        subTotal : "2000",
+      name : "Samsung3",
+      img : "./src/images/img3.avif",
+      unitPrice : "1000",
+      qty : "2",
+      subTotal : "2000",
     },
     {
       id : "114",
-        details:{
-            name : "Samsung4",
-            img : "./src/images/img4.avif",
-
-        },
-        unitPrice : "1000",
-        qty : "2",
-        subTotal : "2000",
+      name : "Samsung4",
+      img : "./src/images/img4.avif",
+      unitPrice : "1000",
+      qty : "2",
+      subTotal : "2000",
     },
     {
       id : "115",
-        details:{
-            name : "Samsung5",
-            img : "./src/images/img5.avif",
-
-        },
-        unitPrice : "1000",
-        qty : "2",
-        subTotal : "2000",
+      name : "Samsung5",
+      img : "./src/images/img5.avif",
+      unitPrice : "1000",
+      qty : "2",
+      subTotal : "2000",
     }
 ]
 
@@ -70,20 +55,20 @@ let products = [
 let readProducts = () => {
   let tbody1 = document.getElementById("tbody1");
 
-  products.forEach((product) => {
+  products.map((product, index) => {
     tbody1.innerHTML += `<tr>
       <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
         <div class="flex">
           <div class="flex-shrink-0 w-10 h-10">
             <img
               class="w-full h-full rounded-full"
-              src="${product.details.img}"
+              src="${product.img}"
               alt="Product Image"
               id="pImg"
             />
           </div>
           <div class="ml-3">
-            <p class="text-gray-900 whitespace-no-wrap" id="pName">${product.details.name}</p>
+            <p class="text-gray-900 whitespace-no-wrap" id="pName">${product.name}</p>
             <p class="text-gray-600 whitespace-no-wrap">${product.id}</p>
           </div>
         </div>
@@ -100,7 +85,7 @@ let readProducts = () => {
         <p class="text-gray-600 whitespace-no-wrap">USD</p>
       </td>
       <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
-        <button onclick="addToCart(${product.id},${product.subTotal})" id="addToCartBtn" type="button" class="inline-block text-yellow-500 hover:text-white p-3 bg-slate-600 rounded">
+        <button onclick="addToCart(${product.img}, ${product.name}, ${product.id}, ${product.unitPrice}, ${product.qty}, ${product.subTotal})" id="addToCartBtn" type="button" class="inline-block text-yellow-500 hover:text-white p-3 bg-slate-600 rounded">
           Add To Cart
         </button>
       </td>
@@ -108,10 +93,8 @@ let readProducts = () => {
   });
 };
 
-
-function addToCart(id, subtotal) {
+function addToCart(img, name, id, unitPrice, qty, subTotal) {
   let tbody2 = document.getElementById("tbody2");
-  console.log(id);
   tbody2.insertAdjacentHTML(
     "beforeend",
     `<tr>
@@ -120,23 +103,32 @@ function addToCart(id, subtotal) {
           <div class="flex-shrink-0 w-10 h-10">
             <img
               class="w-full h-full rounded-full"
-              src="${id}"
+              src="${img}"
               alt="Product Image"
               id="pImg"
             />
           </div>
           <div class="ml-3">
-            <p class="text-gray-900 whitespace-no-wrap" id="pName">${id}</p>
+            <p class="text-gray-900 whitespace-no-wrap" id="pName">${name}</p>
             <p class="text-gray-600 whitespace-no-wrap">${id}</p>
           </div>
         </div>
       </td>
       <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-        <p class="text-gray-900 whitespace-no-wrap">$${subtotal}</p>
+        <p class="text-gray-900 whitespace-no-wrap">$${unitPrice}</p>
+        <p class="text-gray-600 whitespace-no-wrap">USD</p>
+      </td>
+      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p class="text-gray-900 whitespace-no-wrap">${qty}</p>
+      </td>
+      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p class="text-gray-900 whitespace-no-wrap">$${subTotal}</p>
         <p class="text-gray-600 whitespace-no-wrap">USD</p>
       </td>
     </tr>`
   );
 }
+
+
 
 ready();
